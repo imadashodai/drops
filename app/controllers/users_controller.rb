dc5b53feeb36d8fccb7f01be7a441fdd4758c3af
shoday10 <imada@fiilse.com>
2017-06-10 13:12:31 +0900

    modify users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
-  end
-
-  def show
   end
 
   def new
@@ -12,22 +11,26 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    end
+    redirect_to action: :index if @user.save
   end
 
   def edit
   end
 
   def update
+    redirect_to action: :index if @user.update(user_params)
   end
 
   def destroy
+    redirect_to action: :index if @user.delete
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :account_name, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users do
-  end
+  resources :users, except: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
