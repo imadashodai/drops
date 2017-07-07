599ec9b3ff875d59e1edbf18f9a1878ceb35bec0
shoday10 <imada@fiilse.com>
2017-07-08 02:45:00 +0900

    add category in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create([{name: '田中 太郎', account_name: 'taro_1', password: 'taro', password_confirmation: 'taro'}])
+Category.create([{name: 'スポーツ', is_active: 1}, {name: '生活', is_active: 1}])
